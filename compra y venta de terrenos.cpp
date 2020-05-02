55bdd954f1bb5656c9dc7f7d11aa21eccbd95f6f
jonnathan115 <64670927+jonnathan115@users.noreply.github.com>
2020-05-02 13:18:03 -0600

    Add files via upload

---
+++ compra y venta de terrenos.cpp
@@ -0,0 +1,71 @@
+#include<iostream>
+#include<stdio.h>
+#include<stdlid.h>
+#include<string.h>
+#include<conio.h>
+#include<fstream>
+
+using namespace std;
+void compraVentas();
+int main()
+
+struct inmobiliaria
+{
+	int cant;
+	double precio;
+	string nombre_p;
+}
+
+inmob[10];
+char name[78];
+ofstream entrada;
+ofstream entrada1
+ifstream entrada2("inmobiliariajohnn.txt");
+
+float totalv = 0;
+double totala = 0;
+double total = 0, isv = 0;
+char resp;
+int j=0;
+int resp2=1;
+int conj=0;
+void registrar()
+{
+	float totalv = 0;
+	double totalar = 0;
+	double total = 0;
+	strig nomb;
+	int cod;
+	
+	int i=0;
+	int j=0;
+	
+	int cant1=0;
+	int cant11=0;
+	cout<<"ingrese que desea comprar"
+	cin>>resp2;
+		
+
+system("cls");
+
+main();
+}
+
+else
+{
+	for(i=0;i<=resp2;1++)
+	{
+		do
+		{
+			cout<<"ingrese bien que desea comprar: "<<endl;
+			cin>>cod;
+			
+			switch(cod)
+			{
+				case 1: inmob[i].nombre_p=
+					break;
+				}
+			}
+		}
+	}
+}
